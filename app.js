d887a3529e24cda70cc675f3b3f884f0945c91b2
Lilian64 <lilian.mahut@gmail.com>
2019-05-21 10:58:32 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var choixJoueur = "", imageJoueur = "", scoreJoueur = 0, scoreOrdi = 0;
+var boxImageJoueur = document.getElementById("imageJoueur");
+var boxScoreJoueur = document.getElementById("compteurJoueur");
+var choixOrdi = ["pierre", "feuille", "ciseaux"];
+var boxImageOrdi = document.getElementById("imageOrdi");
+var boxScoreOrdi = document.getElementById("compteurOrdi");
+
+function change(joueur) {
+  choixJoueur = joueur;
+  boxImageJoueur.setAttribute("src", "./images/"+joueur+".png");
+  var num = Math.floor(Math.random() * choixOrdi.length);
+  var ordi = choixOrdi[num];
+  boxImageOrdi.setAttribute("src", "./images/"+ordi+".png");
+  if  (joueur == ordi) {
+    boxScoreJoueur.innerHTML ="Score : " + scoreJoueur;
+    boxScoreOrdi.innerHTML ="Score : " + scoreOrdi;
+  } else if (joueur == "pierre" && ordi == "ciseaux" || joueur == "ciseaux" && ordi == "feuille" || joueur == "feuille" && ordi == "pierre")  {
+    scoreJoueur++;
+    boxScoreJoueur.innerHTML ="Score :" + scoreJoueur;
+    boxScoreOrdi.innerHTML ="Score :" + scoreOrdi;
+  } else if (joueur == "pierre" && ordi == "feuille" || joueur == "feuille" && ordi == "ciseaux" || joueur == "ciseaux" && ordi == "pierre")  {
+    scoreOrdi++;
+    boxScoreJoueur.innerHTML ="Score :" + scoreJoueur;
+    boxScoreOrdi.innerHTML ="Score :" + scoreOrdi;
+  }
+}
